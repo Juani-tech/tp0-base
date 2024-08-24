f4f983fc6b7c7e926b0e755c047b6d4859daabb1
Juani <jperezdichiazza@gmail.com>
2024-08-24 18:34:35 -0300

    added: short-read/write handle, graceful shutdown to both, client and server

--- client/common/client.go
+++ client/common/client.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/op/go-logging"
@@ -52,38 +55,85 @@ func (c *Client) createClientSocket() error {
 
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM)
+	stop := make(chan bool, 1)
+
+	// goroutine to handle the signal and trigger shutdown
+	// it has to be goroutine because the channels otherwise would block the program
+	// until SIGTERM is received
+	go func() {
+		sig := <-sigs
+		log.Infof("action: signal_received | result: success | signal: %v | client_id: %v", sig, c.config.ID)
+		stop <- true
+	}()
+
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
 
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
+	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
+		// As tour of go says:
+		// The select statement lets a goroutine wait on multiple communication operations.
+		select {
+		case <-stop:
+			log.Infof("action: loop_terminated | result: interrupted | client_id: %v", c.config.ID)
+			c.conn.Close()
+			return
+		default:
+			c.createClientSocket()
+
+			message := fmt.Sprintf("[CLIENT %v] Message N°%v\n", c.config.ID, msgID)
+			err := c.SendAll(message)
+
+			if err != nil {
+				log.Errorf("action: send_message | result: fail | client_id: %v | error: %v",
+					c.config.ID,
+					err,
+				)
+				return
+			}
+
+			msg, err := bufio.NewReader(c.conn).ReadString('\n')
+			c.conn.Close()
+
+			if err != nil {
+				log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+					c.config.ID,
+					err,
+				)
+				return
+			}
+
+			log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+				c.config.ID,
+				msg,
+			)
+
+			// Wait a time between sending one message and the next one
+			time.Sleep(c.config.LoopPeriod)
+		}
+	}
+	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+}
+
+// Tries to send all the bytes in string, returns the error raised if there is one
+func (c *Client) SendAll(message string) error {
+	log.Infof("message: %s", message)
+	for bytes_sent := 0; bytes_sent < len(message); {
+		bytes, err := fmt.Fprint(
 			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
+			message,
 		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
 
 		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+			log.Errorf("action: send_message | result: fail | client_id: %v | error: %v",
 				c.config.ID,
 				err,
 			)
-			return
+			return err
 		}
 
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
+		bytes_sent += bytes
 	}
-	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+	return nil
 }
--- server/common/server.py
+++ server/common/server.py
@@ -1,13 +1,21 @@
 import socket
 import logging
+import signal
+import time
 
 
 class Server:
+
     def __init__(self, port, listen_backlog):
         # Initialize server socket
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(("", port))
         self._server_socket.listen(listen_backlog)
+        self._sigterm_received = False
+
+    def _sigterm_handler(self, signum, frames):
+        self._sigterm_received = True
+        self._server_socket.shutdown(socket.SHUT_RDWR)  # No further writes/read allowed
 
     def run(self):
         """
@@ -20,28 +28,58 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        signal.signal(signal.SIGTERM, self._sigterm_handler)
+
+        while not self._sigterm_received:
+            try:
+                client_sock = self.__accept_new_connection()
+                self.__handle_client_connection(client_sock)
+            except OSError as e:
+                logging.error(
+                    "action: accept_new_connections | result: fail | error: {e}"
+                )
 
     def __handle_client_connection(self, client_sock):
         """
-        Read message from a specific client socket and closes the socket
+        Read message from a specific client socket and close the socket.
 
         If a problem arises in the communication with the client, the
-        client socket will also be closed
+        client socket will also be closed.
         """
+
+        def recv_all(sock, buffer_size):
+            """Receive all data from the socket, handling short-reads."""
+            data = b""
+            while True:
+                part = sock.recv(buffer_size)
+                if len(part) == 0:
+                    # The other side closed the connection
+                    raise OSError
+                data += part
+                if len(part) < buffer_size:
+                    # End of the message
+                    break
+            return data
+
+        def send_all(sock, data):
+            """Send all data through the socket, handling short-writes."""
+            total_sent = 0
+            while total_sent < len(data):
+                sent = sock.send(data[total_sent:])
+                if sent == 0:
+                    raise OSError
+                total_sent += sent
+
         try:
-            # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode("utf-8")
+            msg = recv_all(client_sock, 1024).rstrip().decode("utf-8")
             addr = client_sock.getpeername()
             logging.info(
                 f"action: receive_message | result: success | ip: {addr[0]} | msg: {msg}"
             )
-            # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode("utf-8"))
+            time.sleep(5)
+            send_all(client_sock, "{}\n".format(msg).encode("utf-8"))
         except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
+            logging.error(f"action: receive_message | result: fail | error: {e}")
         finally:
             client_sock.close()
 
