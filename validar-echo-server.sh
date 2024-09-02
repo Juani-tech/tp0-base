23d805e89645f36e42847ff9b041b5a2433f9bac
Juani <jperezdichiazza@gmail.com>
2024-09-02 15:47:41 -0300

    add docker-compose-validator-down to script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make docker-compose-validator-up && make docker-compose-validator-logs
+make docker-compose-validator-up && make docker-compose-validator-logs && make docker-compose-validator-down 
