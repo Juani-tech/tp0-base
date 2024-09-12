12a224bda5f8a88f8aec37db594f00c885cbfb58
Juani <jperezdichiazza@gmail.com>
2024-09-12 11:05:38 -0300

    fix possible short read in SafeSocket

--- client/communication/safeSocket.go
+++ client/communication/safeSocket.go
@@ -52,53 +52,86 @@ func (s *SafeSocket) SendAll(message string) error {
 	return nil
 }
 
-func (s *SafeSocket) RecvAllWithLengthBytes() (string, error) {
-	totalMessage := ""
+func (s *SafeSocket) readLength() (int, error) {
+	lengthBuffer := make([]byte, 0)
+	for len(lengthBuffer) < s.lengthBytes {
+		select {
+		case <-s.stop:
+			return 0, errors.New("sigterm received")
+		default:
+			s.conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
 
-	for {
+			reader := bufio.NewReader(s.conn)
+			buffer := make([]byte, s.lengthBytes)
+
+			bytesRead, err := reader.Read(buffer)
+
+			s.conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+			if err != nil || bytesRead != s.lengthBytes {
+				if errors.Is(err, os.ErrDeadlineExceeded) {
+					continue
+				}
+				return 0, err
+			}
+
+			if bytesRead > 0 {
+				lengthBuffer = append(lengthBuffer, buffer[:bytesRead]...)
+			}
+		}
+	}
+	str := string(lengthBuffer)
+
+	length, err := strconv.Atoi(str)
+	if err != nil {
+		return 0, err
+	}
+
+	return length, nil
+}
+
+func (s *SafeSocket) readNBytes(n int) (string, error) {
+	totalMessage := ""
+	reader := bufio.NewReader(s.conn)
+
+	for len(totalMessage) < n {
 		select {
 		case <-s.stop:
 			return "", errors.New("sigterm received")
 		default:
 			s.conn.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
 
-			reader := bufio.NewReader(s.conn)
-			buffer := make([]byte, s.lengthBytes)
-			bytesRead, err := reader.Read(buffer)
-
-			if err != nil || bytesRead != s.lengthBytes {
-				if errors.Is(err, os.ErrDeadlineExceeded) {
-					continue
-				}
-				return "", err
-			}
+			msgBuffer := make([]byte, n)
+			bytesRead, err := reader.Read(msgBuffer)
 
 			s.conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 
-			str := string(buffer)
-
-			length, err := strconv.Atoi(str)
 			if err != nil {
-				return "", err
-			}
-
-			msgBuffer := make([]byte, length)
-			bytesRead, err = reader.Read(msgBuffer)
-
-			if err != nil || bytesRead < length {
 				return "", err
 			}
 
 			// Accumulate the message buffer into totalMessage
 			totalMessage += string(msgBuffer[:bytesRead])
 
-			// Check if the accumulated message contains a newline
-			if strings.Contains(totalMessage, "\n") {
-				// Trim the \n and return the full message
-				return strings.TrimSuffix(totalMessage, "\n"), nil
-			}
 		}
 	}
+	// Trim the \n and return the full message
+	return strings.TrimSuffix(totalMessage, "\n"), nil
+}
+
+func (s *SafeSocket) RecvAllWithLengthBytes() (string, error) {
+	length, err := s.readLength()
+	if err != nil {
+		return "", err
+	}
+	message, err := s.readNBytes(length)
+	if err != nil {
+		return "", err
+	}
+
+	log.Debugf("Received: %s", message)
+
+	return message, nil
 }
 
 func (s *SafeSocket) Close() {
--- server/communication/safe_socket.py
+++ server/communication/safe_socket.py
@@ -1,31 +1,68 @@
-class SafeSocket: 
-    def __init__(self, sock, length_bytes): 
+class SafeSocket:
+    def __init__(self, sock, length_bytes):
         self._sock = sock
         self._length_bytes = length_bytes
 
+    def __receive_length(self):
+        """
+        Raises:
+            OSError: if the amount of read bytes is 0 (zero), which indicates that the socket is closed
+
+        Returns:
+            int: the length decoded (utf-8) as int
+        """
+        received = b""
+        while len(received) < 6:
+            received += self._sock.recv(self._length_bytes)
+            if len(received) == 0:
+                raise OSError
+
+        return int(received.decode("utf-8"))
+
+    def __receive_message(self, message_len):
+        """_summary_
+
+        Args:
+            message_len (int): length of the message in bytes
+
+        Raises:
+            OSError: if the amount of read bytes is 0 (zero), which indicates that the socket is closed
+
+        Returns:
+            the message decoded (utf-8) as string
+        """
+        received = b""
+        while len(received) < message_len:
+            received += self._sock.recv(message_len)
+
+            if len(received) == 0:
+                raise OSError
+
+        return received
+
     def recv_all_with_length_bytes(self):
         """Receive all data from the socket, handling short-reads."""
-        data = b""
-        while True:
-            length = self._sock.recv(self._length_bytes).decode('utf-8')
-            
-            if len(length) == 0:
-                # The other side closed the connection
-                raise OSError
-            
-            part = self._sock.recv(int(length)) 
-            
-            if len(part) == 0:
-                # The other side closed the connection
-                raise OSError
-            
+        # data = b""
+        # while True:
+        # length = self._sock.recv(self._length_bytes).decode("utf-8")
+        length = self.__receive_length()
+        message = self.__receive_message(length)
+        # if len(length) == 0:
+        #     # The other side closed the connection
+        #     raise OSError
 
-            data += part
+        # part = self._sock.recv(int(length))
 
-            if b"\n" in part:
-                # End of the message
-                break
-        return data
+        # if len(part) == 0:
+        #     # The other side closed the connection
+        #     raise OSError
+
+        # data += part
+
+        # if b"\n" in part:
+        # End of the message
+        # break
+        return message
 
     def close(self):
         self._sock.close()
